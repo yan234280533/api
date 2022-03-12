b77afc8c920af9e8eeb88ee248b839154d0b16ef
devinyan <devinyan@tencent.com>
2022-03-12 20:35:38 +0800

    add qos and priority class defination for ensurance

--- ensurance/v1alpha1/types.go
+++ ensurance/v1alpha1/types.go
@@ -15,6 +15,86 @@ const (
 	AvoidanceActionStrategyPreview AvoidanceActionStrategy = "Preview"
 )
 
+type CPUSetStrategy string
+
+const (
+	CPUSetStrategyNone   CPUSetStrategy = "None"
+	CPUSetStrategyStatic CPUSetStrategy = "Static"
+	CPUSetStrategyShare  CPUSetStrategy = "Share"
+)
+
+// +genclient
+// +genclient:nonNamespaced
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:shortName=ql
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PriorityLevel defines the qos behaviours for this priority's pods
+type PriorityLevel struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec PriorityLevelSpec `json:"spec"`
+
+	Status PriorityLevelStatus `json:"status,omitempty"`
+}
+
+type PriorityLevelSpec struct {
+	// PriorityClassName the priority name associated.
+	// +required
+	PriorityClassName string `json:"priorityClassName"`
+
+	// ResourcePriority define the priority for various resources
+	// +optional
+	ResourcePriority ResourcePriority `json:"ResourcePriority,omitempty"`
+
+	// AvoidanceStrategy define the avoidance strategy for pods
+	// +optional
+	AvoidanceStrategy AvoidanceStrategy `json:"AvoidanceStrategy,omitempty"`
+
+	// 是否需要扩展资源
+	// ResourceRequestMutations
+}
+
+// PriorityLevelStatus defines the desired status of PriorityLevel
+type PriorityLevelStatus struct {
+}
+
+type ResourcePriority struct {
+	CPUPriority       CPUPriority       `json:"cpuPriority,omitempty"`
+	MemoryPriority    MemoryPriority    `json:"memoryPriority,omitempty"`
+	NetworkIOPriority NetworkIOPriority `json:"networkIOPriority,omitempty"`
+}
+
+type CPUPriority struct {
+	Priority int32 `json:"priority,omitempty"`
+}
+
+type MemoryPriority struct {
+	Priority       int32 `json:"priority,omitempty"`
+	WatermarkRatio int32 `json:"watermarkRatio,omitempty"`
+}
+
+type NetworkIOPriority struct {
+	Priority int32 `json:"priority,omitempty"`
+}
+
+type AvoidanceStrategy struct {
+	AllowThrottle bool `json:"allowThrottle,omitempty"`
+	AllowEvict    bool `json:"allowEvict,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PriorityLevelList contains a list of PriorityLevel
+type PriorityLevelList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PriorityLevel `json:"items"`
+}
+
 // PodQOSEnsurancePolicySpec defines the desired status of PodQOSEnsurancePolicy
 type PodQOSEnsurancePolicySpec struct {
 	// Selector is a label query over pods that should match the policy
@@ -25,20 +105,57 @@ type PodQOSEnsurancePolicySpec struct {
 
 	// ObjectiveEnsurances is an array of ObjectiveEnsurance
 	ObjectiveEnsurances []ObjectiveEnsurance `json:"objectiveEnsurance,omitempty"`
+
+	ResourceQos ResourceQos `json:"resourceQos,omitempty"`
+}
+
+type ResourceQos struct {
+	CPUQos       CPUQos       `json:"cpuQos,omitempty"`
+	MemoryQos    MemoryQos    `json:"memoryQos,omitempty"`
+	NetworkIOQos NetworkIOQos `json:"networkIOQos,omitempty"`
+	DiskIOQos    DiskIOQos    `json:"diskIOQos,omitempty"`
+	RDTQos       RDTQos       `json:"rdtQos,omitempty"`
+}
+
+type CPUQos struct {
+	CPUSetStrategy      CPUSetStrategy    `json:"cpuSetStrategy,omitempty"`
+	HyperThreadingAvoid bool              `json:"htAvoid,omitempty"`
+	BurstEnable         bool              `json:"burstEnable,omitempty"`
+	BurstQuota          resource.Quantity `json:"burstQuota,omitempty"`
+}
+
+type MemoryQos struct {
+	AsyncRatio          int32 `json:"asyncRatio,omitempty"`
+	AsyncDistanceFactor int32 `json:"asyncDistanceFactor,omitempty"`
+	OomGroup            bool  `json:"oomGroup,omitempty"`
+}
+
+type NetworkIOQos struct {
+	RxBps int32 `json:"rxBps,omitempty"`
+	TxBps int32 `json:"txBps,omitempty"`
+}
+
+type DiskIOQos struct {
+	ReadIOPS  int32 `json:"readIOPS,omitempty"`
+	WriteIOPS int32 `json:"writeIOPS,omitempty"`
+	ReadBPS   int32 `json:"readBps,omitempty"`
+	WriteBPS  int32 `json:"writeBps,omitempty"`
+}
+
+type RDTQos struct {
+	LLCBits  int32 `json:"llcBits,omitempty"`
+	MBARatio int32 `json:"mbaRatio,omitempty"`
 }
 
 type QualityProbe struct {
 	// HTTPGet specifies the http request to perform.
 	// +optional
 	HTTPGet *corev1.HTTPGetAction `json:"httpGet,omitempty"`
-	// Init delay time for handler
-	// Defaults to 5
+
+	// TimeoutSeconds is the timeout for request
+	// Defaults to 60, instead not timeout
 	// +optional
-	InitialDelaySeconds *int32 `json:"initialDelaySeconds,omitempty"`
-	// Timeout for request.
-	// Defaults to 0, instead not timeout
-	// +optional
-	TimeoutSeconds *int32 `json:"timeoutSeconds,omitempty"`
+	TimeoutSeconds int32 `json:"timeoutSeconds,omitempty"`
 }
 
 // PodQOSEnsurancePolicyStatus defines the observed status of PodQOSEnsurancePolicy
@@ -91,6 +208,26 @@ type NodeQOSEnsurancePolicySpec struct {
 
 	// ObjectiveEnsurances is an array of ObjectiveEnsurance
 	ObjectiveEnsurances []ObjectiveEnsurance `json:"objectiveEnsurances,omitempty"`
+
+	// ResourceWatermark define the resource watermark for the node
+	// +optional
+	ResourceWatermark ResourceWatermark `json:"resourceWatermark,omitempty"`
+}
+
+type ResourceWatermark struct {
+	CPUWatermark       CPUWatermark       `json:"cpuWatermark,omitempty"`
+	NetworkIOWatermark NetworkIOWatermark `json:"networkIOWatermark,omitempty"`
+}
+
+type CPUWatermark struct {
+	WatermarkCPURatio int32 `json:"watermarkCPURatio,omitempty"`
+}
+
+type NetworkIOWatermark struct {
+	RxBpsMax int32 `json:"rxBpsMax,omitempty"`
+	RxBpsMin int32 `json:"rxBpsMin,omitempty"`
+	TxBpsMax int32 `json:"txBpsMax,omitempty"`
+	TxBpsMin int32 `json:"txBpsMin,omitempty"`
 }
 
 type NodeQualityProbe struct {
